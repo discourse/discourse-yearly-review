208fe577a2d65990288ad257fc9e53ebc14c55c2
scossar <scossar3@gmail.com>
2018-12-28 13:15:42 -0800

    Test that the job is scheduled correctly

--- spec/jobs/yearly_review_spec.rb
+++ spec/jobs/yearly_review_spec.rb
@@ -2,124 +2,57 @@ require 'rails_helper'
 
 # Todo: make sure tests won't fail in January.
 describe Jobs::YearlyReview do
-  describe 'query results' do
-    let (:review_creator) { Fabricate(:user, admin: true) }
-    let (:top_review_user) { Fabricate(:user, username: 'top_review_user') }
-    let(:review_user) { Fabricate(:user, username: 'review_user') }
+  let(:top_review_user) { Fabricate(:user, username: 'top_review_user') }
+  let(:reviewed_user) { Fabricate(:user, username: 'review_user') }
 
-    context 'most topics' do
+  describe 'publishing the topic' do
+    context 'January 1, 2019' do
       before do
-        freeze_time DateTime.parse('2018-02-01')
-        5.times { Fabricate(:topic, user: top_review_user) }
-        Fabricate(:topic, user: review_user)
+        freeze_time DateTime.parse('2019-01-01')
+        Fabricate(:topic, created_at: 1.month.ago)
       end
+      it 'publishes a review topic' do
+        Jobs::YearlyReview.new.execute({})
+        topic = Topic.last
 
-      it 'should rank the users correctly' do
-        Jobs::YearlyReview.new.execute(review_user: review_creator)
-        topic = Topic.where(archetype: 'regular').last
-        raw = Post.where(topic_id: topic.id).first.raw
-
-        expect(raw).to have_tag('table.topics-created tr.user-row-0') { with_tag('td', text: /5/) }
-        expect(raw).to have_tag('table.topics-created tr.user-row-1') { with_tag('td', text: /1/) }
+        expect(topic.title).to eq(I18n.t('yearly_review.topic_title', year: 2018))
       end
     end
 
-    context 'most replies' do
-      let(:reviewed_topic) { Fabricate(:topic) }
-      it 'should rank the users correctly' do
-        freeze_time DateTime.parse('2018-02-01')
-        5.times do
-          Fabricate(:post, topic: reviewed_topic, user: top_review_user)
-        end
-        Fabricate(:post, topic: reviewed_topic, user: review_user)
-
-        Jobs::YearlyReview.new.execute(review_user: review_creator)
-        topic = Topic.where(archetype: 'regular').last
-        raw = Post.where(topic_id: topic.id).first.raw
-
-        expect(raw).to have_tag('table.replies-created tr.user-row-0') { with_tag('td', text: /4/) }
-        expect(raw).to have_tag('table.replies-created tr.user-row-1') { with_tag('td', text: /1/) }
-      end
-    end
-
-    context 'likes given and received' do
-      let(:reviewed_topic) { Fabricate(:topic) }
+    context 'February 1, 2019' do
       before do
-        freeze_time DateTime.parse('2018-02-01')
-        5.times do
-          post = Fabricate(:post, topic: reviewed_topic, user: review_user)
-          UserAction.create!(action_type: PostActionType.types[:like],
-                             user_id: review_user.id,
-                             acting_user_id: top_review_user.id,
-                             target_post_id: post.id,
-                             target_topic_id: reviewed_topic.id)
-        end
-        post = Fabricate(:post, topic: reviewed_topic, user: top_review_user)
-        UserAction.create!(action_type: PostActionType.types[:like],
-                           user_id: top_review_user.id,
-                           acting_user_id: review_user.id,
-                           target_post_id: post.id,
-                           target_topic_id: reviewed_topic.id)
+        freeze_time DateTime.parse('2019-02-01')
+        Fabricate(:topic, created_at: 2.months.ago, title: 'A topic from 2018')
       end
-      it 'should rank the users correctly' do
-        Jobs::YearlyReview.new.execute(review_user: review_creator)
-        topic = Topic.where(archetype: 'regular').last
-        raw = Post.where(topic_id: topic.id).first.raw
+      it "doesn't publish a review topic" do
+        Jobs::YearlyReview.new.execute({})
+        topic = Topic.last
 
-        expect(raw).to have_tag('table.likes-given tr.user-row-0') { with_tag('td', text: /5/) }
-        expect(raw).to have_tag('table.likes-given tr.user-row-0') { with_tag('td', text: /top_review_user/) }
-        expect(raw).to have_tag('table.likes-given tr.user-row-1') { with_tag('td', text: /1/) }
-        expect(raw).to have_tag('table.likes-given tr.user-row-1') { with_tag('td', text: /review_user/) }
-
-        expect(raw).to have_tag('table.likes-received tr.user-row-0') { with_tag('td', text: /5/) }
-        expect(raw).to have_tag('table.likes-received tr.user-row-0') { with_tag('td', text: /review_user/) }
-        expect(raw).to have_tag('table.likes-received tr.user-row-1') { with_tag('td', text: /1/) }
-        expect(raw).to have_tag('table.likes-received tr.user-row-1') { with_tag('td', text: /top_review_user/) }
-      end
-    end
-
-    context 'featured badge users' do
-      SiteSetting.yearly_review_featured_badge = 'Editor'
-      before do
-        freeze_time DateTime.parse('2018-02-01')
-        badge = Badge.find_by(name: SiteSetting.yearly_review_featured_badge)
-        5.times do
-          user = Fabricate(:user)
-          BadgeGranter.grant(badge, user)
-        end
-      end
-      it 'features the correct number of users' do
-        Jobs::YearlyReview.new.execute(review_user: review_creator)
-        topic = Topic.where(archetype: 'regular').last
-        raw = Post.where(topic_id: topic.id).first.raw
-
-        expect(raw).to have_tag('a.mention', count: 5)
+        expect(topic.title).to eq('A topic from 2018')
       end
     end
   end
 
-  describe 'review topic settings' do
-    let(:review_creator) { Fabricate(:user, admin: true) }
-    let(:regular_user) { Fabricate(:user, username: 'regular_user') }
-    let(:unreviewed_user) { Fabricate(:user, username: 'unreviewed_user') }
-    let(:review_category) { Fabricate(:category, name: 'scratch') }
-    let(:non_review_category) { Fabricate(:category, name: 'sniff') }
-    let(:review_topic) { Fabricate(:topic, title: 'A well liked topic', user: regular_user, category_id: review_category.id) }
-    let(:non_review_topic) { Fabricate(:topic, title: 'This topic will not be reviewed', user: unreviewed_user, category_id: non_review_category.id) }
+  describe 'review categories' do
+    before do
+      freeze_time DateTime.parse('2019-01-01')
+    end
+    it 'only displays data from public categories' do
+      review_category = Fabricate(:category)
+      non_review_category = Fabricate(:category)
+      group = Fabricate(:group)
+      non_review_category.set_permissions(group => :full)
+      non_review_category.save
+      Fabricate(:topic, category_id: non_review_category.id, user: reviewed_user, created_at: 1.month.ago)
+      Fabricate(:topic, category_id: review_category.id, user: top_review_user, created_at: 1.month.ago)
 
-    it 'should respect the review_categories setting' do
-      SiteSetting.yearly_review_categories = "#{review_category.id}"
-      review_category
-      non_review_category
-      review_topic
-      non_review_topic
+      Jobs::YearlyReview.new.execute({})
+      topic = Topic.last
+      raw = Post.where(topic_id: topic.id).first.raw
 
-      Jobs::YearlyReview.new.execute(review_user: review_creator)
-      topic = Topic.where(archetype: 'regular').last
-      post = Post.where(topic_id: topic.id).first
-
-      expect(post.raw).to have_tag('a', text: '@regular_user')
-      expect(post.raw).not_to have_tag('a', text: '@unreviewed_user')
+      expect(raw).not_to have_tag('td', text: /@review_user/)
+      expect(raw).to have_tag('td', text: /@top_review_user/)
     end
   end
+
 end

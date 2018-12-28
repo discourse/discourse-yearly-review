7bd6a35ffaf291f12134fe47a789e999119fcab7
scossar <scossar3@gmail.com>
2018-12-28 15:52:04 -0800

    Add tests for user and topic stats

--- spec/jobs/yearly_review_spec.rb
+++ spec/jobs/yearly_review_spec.rb
@@ -1,10 +1,8 @@
 require 'rails_helper'
 
-# Todo: make sure tests won't fail in January.
 describe Jobs::YearlyReview do
   let(:top_review_user) { Fabricate(:user, username: 'top_review_user') }
-  let(:reviewed_user) { Fabricate(:user, username: 'review_user') }
-
+  let(:reviewed_user) { Fabricate(:user, username: 'reviewed_user') }
   describe 'publishing the topic' do
     context 'January 1, 2019' do
       before do
@@ -50,9 +48,143 @@ describe Jobs::YearlyReview do
       topic = Topic.last
       raw = Post.where(topic_id: topic.id).first.raw
 
-      expect(raw).not_to have_tag('td', text: /@review_user/)
+      expect(raw).not_to have_tag('td', text: /@reviewed_user/)
       expect(raw).to have_tag('td', text: /@top_review_user/)
     end
   end
 
+  describe 'user stats' do
+    before do
+      freeze_time DateTime.parse('2019-01-01')
+    end
+
+    context 'most topics' do
+      before do
+        5.times { Fabricate(:topic, user: top_review_user, created_at: 1.month.ago) }
+        Fabricate(:topic, user: reviewed_user, created_at: 1.month.ago)
+      end
+
+      it 'ranks topics created by users correctly' do
+        Jobs::YearlyReview.new.execute({})
+        topic = Topic.last
+        raw = Post.where(topic_id: topic.id).first.raw
+
+        expect(raw).to have_tag('table.topics-created tr.user-row-0') { with_tag('td', text: /5/) }
+        expect(raw).to have_tag('table.topics-created tr.user-row-1') { with_tag('td', text: /1/) }
+      end
+    end
+
+    context 'most replies' do
+      before do
+        SiteSetting.max_consecutive_replies = 5
+        topic_user = Fabricate(:user)
+        reviewed_topic = Fabricate(:topic, user: topic_user, created_at: 1.year.ago)
+        Fabricate(:post, topic: reviewed_topic, user: topic_user)
+        5.times { Fabricate(:post, topic: reviewed_topic, user: top_review_user, created_at: 1.month.ago) }
+        Fabricate(:post, topic: reviewed_topic, user: reviewed_user, created_at: 1.month.ago)
+      end
+
+      it 'ranks replies created by users correctly' do
+        Jobs::YearlyReview.new.execute({})
+        topic = Topic.last
+        raw = Post.where(topic_id: topic.id).first.raw
+
+        expect(raw).to have_tag('table.replies-created tr.user-row-0') { with_tag('td', text: /5/) }
+        expect(raw).to have_tag('table.replies-created tr.user-row-1') { with_tag('td', text: /1/) }
+      end
+    end
+
+    context 'likes given and received' do
+      let(:reviewed_topic) { Fabricate(:topic, created_at: 1.year.ago) }
+      before do
+        5.times do
+          post = Fabricate(:post, topic: reviewed_topic, user: reviewed_user, created_at: 1.month.ago)
+          UserAction.create!(action_type: PostActionType.types[:like],
+                             user_id: reviewed_user.id,
+                             acting_user_id: top_review_user.id,
+                             target_post_id: post.id,
+                             target_topic_id: reviewed_topic.id,
+                             created_at: 1.month.ago)
+        end
+        post = Fabricate(:post, topic: reviewed_topic, user: top_review_user, created_at: 1.month.ago)
+        UserAction.create!(action_type: PostActionType.types[:like],
+                           user_id: top_review_user.id,
+                           acting_user_id: reviewed_user.id,
+                           target_post_id: post.id,
+                           target_topic_id: reviewed_topic.id,
+                           created_at: 1.month.ago)
+      end
+
+      it 'should rank likes given and received correctly' do
+        Jobs::YearlyReview.new.execute({})
+        topic = Topic.last
+        raw = Post.where(topic_id: topic.id).first.raw
+
+        expect(raw).to have_tag('table.likes-given tr.user-row-0') { with_tag('td', text: /5/) }
+        expect(raw).to have_tag('table.likes-given tr.user-row-0') { with_tag('td', text: /@top_review_user/) }
+        expect(raw).to have_tag('table.likes-given tr.user-row-1') { with_tag('td', text: /1/) }
+        expect(raw).to have_tag('table.likes-given tr.user-row-1') { with_tag('td', text: /@reviewed_user/) }
+
+        expect(raw).to have_tag('table.likes-received tr.user-row-0') { with_tag('td', text: /5/) }
+        expect(raw).to have_tag('table.likes-received tr.user-row-0') { with_tag('td', text: /@reviewed_user/) }
+        expect(raw).to have_tag('table.likes-received tr.user-row-1') { with_tag('td', text: /1/) }
+        expect(raw).to have_tag('table.likes-received tr.user-row-1') { with_tag('td', text: /@top_review_user/) }
+      end
+    end
+  end
+
+  describe 'topic stats' do
+    before do
+      freeze_time DateTime.parse('2019-01-01')
+    end
+    context 'most liked topics' do
+      before do
+        most_liked = Fabricate(:topic, title: 'The most liked topic', created_at: 1.month.ago)
+        second_most_liked = Fabricate(:topic, title: 'The second most liked topic', created_at: 1.month.ago)
+        most_liked_post = Fabricate(:post, topic: most_liked, created_at: 1.month.ago)
+        second_most_liked_post = Fabricate(:post, topic: second_most_liked, created_at: 1.month.ago)
+        5.times do
+          PostAction.create!(post_id: most_liked_post.id,
+                             user_id: Fabricate(:user).id,
+                             post_action_type_id: PostActionType.types[:like],
+                             created_at: 1.month.ago)
+        end
+        PostAction.create!(post_id: second_most_liked_post.id,
+                           user_id: Fabricate(:user).id,
+                           post_action_type_id: PostActionType.types[:like],
+                           created_at: 1.month.ago)
+
+      end
+
+      it 'ranks the most liked topics correctly' do
+        Jobs::YearlyReview.new.execute({})
+        topic = Topic.last
+        raw = Post.where(topic_id: topic.id).first.raw
+
+        expect(raw).to match(/the-most-liked-topic.+\r\r.+the-second-most-liked-topic/)
+      end
+    end
+
+    context 'most replied to topics' do
+      let(:most_replies) { Fabricate(:topic, created_at: 1.year.ago, title: 'The most replied to topic') }
+      let(:second_most_replies) { Fabricate(:topic, created_at: 1.year.ago, title: 'The second most replied to topic') }
+      before do
+        5.times do
+          Fabricate(:post, topic: most_replies, created_at: 1.month.ago)
+        end
+        2.times do
+          Fabricate(:post, topic: second_most_replies, created_at: 1.month.ago)
+        end
+      end
+
+      it 'ranks the most replied to topics correctly' do
+        Jobs::YearlyReview.new.execute({})
+        topic = Topic.last
+        raw = Post.where(topic_id: topic.id).first.raw
+        puts raw
+
+        expect(raw).to match(/the-most-replied-to-topic.+\r\r.+the-second-most-replied-to-topic/)
+      end
+    end
+  end
 end

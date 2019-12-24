6c555bde2cebc37311f31f0fd4265835f1af6484
scossar <scossar3@gmail.com>
2019-12-24 15:01:36 -0800

    Use register_plugin_style to fix display of tables in emails generated by the plugin

--- plugin.rb
+++ plugin.rb
@@ -23,10 +23,57 @@ after_initialize do
     end
   end
 
-  ::ActionController::Base.prepend_view_path File.expand_path("../app/views/yearly-review", __FILE__)
+  ::ActionController::Base.prepend_view_path File.expand_path('../app/views/yearly-review', __FILE__)
 
   [
     '../../discourse-yearly-review/app/jobs/yearly_review.rb'
   ].each { |path| load File.expand_path(path, __FILE__) }
 
+  require_dependency 'email/styles'
+  Email::Styles.register_plugin_style do |doc|
+    doc.css('[data-review-topic-users] table').each do |element|
+      element['width'] = '100%'
+    end
+    doc.css('[data-review-featured-topics] table').each do |element|
+      element['width'] = '100%'
+    end
+
+    doc.css('[data-review-topic-users] th').each do |element|
+      element['style'] = 'text-align:left;padding-bottom:12px;'
+      element['width'] = '50%'
+    end
+    doc.css('[data-review-featured-topics] th').each do |element|
+      element['style'] = 'text-align:left;padding-bottom:12px;'
+    end
+    doc.css('[data-review-featured-topics] th:first-child').each do |element|
+      element['width'] = '10%'
+    end
+    doc.css('[data-review-featured-topics] th:nth-child(2)').each do |element|
+      element['width'] = '60%'
+    end
+    doc.css('[data-review-featured-topics] th:last-child').each do |element|
+      element['width'] = '30%'
+    end
+
+    doc.css('[data-review-topic-users] td').each do |element|
+      element['style'] = 'padding-bottom: 6px;'
+      element['valign'] = 'top'
+    end
+    doc.css('[data-review-featured-topics] td').each do |element|
+      element['style'] = 'padding-bottom: 6px;'
+      element['valign'] = 'top'
+    end
+    doc.css('[data-review-featured-topics] td:nth-child(2)').each do |element|
+      element['style'] = 'padding-bottom: 6px;padding-right:6px;'
+      element['valign'] = 'top'
+    end
+
+    doc.css('[data-review-topic-users] td table td:first-child').each do |element|
+      element['style'] = 'padding-bottom:6px;'
+        element['width'] = '25'
+    end
+    doc.css('[data-review-topic-users] td table td:nth-child(2)').each do |element|
+      element['style'] = 'padding-left:4px;padding-bottom:6px;'
+    end
+  end
 end

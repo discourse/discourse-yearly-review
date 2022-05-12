38d0d01e03e3bbdc2e3a660b02db4d4dffb74888
Martin Brennan <martin@discourse.org>
2022-05-12 16:44:41 +1000

    FIX: Add polymorphic bookmark support (#34)
    The core PR https://github.com/discourse/discourse/pull/16729
    will not pass unless we change this spec and the job

--- app/jobs/yearly_review.rb
+++ app/jobs/yearly_review.rb
@@ -550,7 +550,13 @@ module ::Jobs
     end
 
     def most_bookmarked_topic_sql
-      <<~SQL
+      post_bookmark_join_sql = \
+        if SiteSetting.use_polymorphic_bookmarks
+          "ON p.id = bookmarks.bookmarkable_id AND bookmarks.bookmarkable_type = 'Post'"
+        else
+          "ON p.id = bookmarks.post_id"
+        end
+    <<~SQL
         SELECT
         username,
         uploaded_avatar_id,
@@ -565,7 +571,7 @@ module ::Jobs
         'bookmarks' AS action
         FROM bookmarks
         JOIN posts p
-        ON p.id = bookmarks.post_id
+        #{post_bookmark_join_sql}
         JOIN topics t
         ON t.id = p.topic_id
         JOIN categories c

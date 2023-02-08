92c92ab14cf15afcc4d63ccf83ad599b58812e13
Vinoth Kannan <svkn.87@gmail.com>
2023-02-08 18:48:44 +0530

    Update plugin.rb
    Co-authored-by: Régis Hanol <regis@hanol.fr>

--- plugin.rb
+++ plugin.rb
@@ -81,7 +81,7 @@ after_initialize do
   end
 
   DiscourseEvent.on(:username_changed) do |old_username, new_username|
-    Post.joins("INNER JOIN post_custom_fields pcf ON pcf.post_id = posts.id").where("pcf.name = ?", YearlyReview::POST_CUSTOM_FIELD).each do |post|
+    Post.joins(:_custom_fields).where("post_custom_fields.name = ?", YearlyReview::POST_CUSTOM_FIELD).find_each do |post|
       post.raw = post.raw.gsub("/#{old_username}/#{YearlyReviewHelper::AVATAR_SIZE}/", "/#{new_username}/#{YearlyReviewHelper::AVATAR_SIZE}/")
       post.save!
     end

87ea9437f456bb69964f44efd946a52ce435a0b4
Vinoth Kannan <svkn.87@gmail.com>
2023-02-08 19:36:57 +0530

    DEV: add a migration to backfill post custom fields to old topics.

---
+++ db/post_migrate/20230208123631_backfill_yearly_review_custom_fields.rb
@@ -0,0 +1,16 @@
+# frozen_string_literal: true
+
+class BackfillYearlyReviewCustomFields < ActiveRecord::Migration[6.1]
+  def change
+    2017.upto(2022) do |year|
+      topic_title = I18n.t("yearly_review.topic_title", year: review_year)
+      DB.exec(<<~SQL, title: topic_title, user_id: Discourse::SYSTEM_USER_ID, name: YearlyReview::POST_CUSTOM_FIELD, value: year.to_s)
+        INSERT INTO post_custom_fields (post_id, name, value, created_at, updated_at)
+        SELECT id, :name, :value, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP
+        FROM posts
+        INNER JOIN topics ON topics.id = posts.topic_id
+        WHERE posts.user_id = :user_id AND topics.title = :title
+      SQL
+    end
+  end
+end
